beedb7a7eb0b05ddaf5c9c87f2834b0dbbd8417c
FiifiGenI <emmanuel.essel@generation.org>
2024-09-22 19:35:57 +0000

    strong password checker

---
+++ javascript/if.js
@@ -0,0 +1,31 @@
+// Write a program that checks if a password meets the following criteria:
+
+// At least 8 characters long.
+// Contains at least one uppercase letter.
+// Contains at least one lowercase letter.
+// Contains at least one digit.
+// If the password meets all criteria, print "Password is strong." Otherwise, print "Password is weak."
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+
+const prompt = require("prompt-sync")();
+
+let userPassword = prompt("Enter password: ");
+function isUpper() {
+    return /[A-Z]/.test(userPassword);
+}
+function isLower() {
+    return /[a - z]/.test(userPassword);
+}
+function isDigit() {
+    return /\d/.test(userPassword);
+}
+function checklength() {
+    return userPassword.length >= 8;
+}
+
+if(checklength && isDigit() && isLower() && isUpper()) {
+    console.log("You have a strong Password")
+} else {
+    console.log("Password is weak!")
+}
