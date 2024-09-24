4667fb30c562cc9bc9a6c88284739aec1ef3c170
FiifiGenI <emmanuel.essel@generation.org>
2024-09-24 14:18:45 +0000

    Using ternary instead of ifs

--- javascript/rockpaperscissors.js
+++ javascript/rockpaperscissors.js
@@ -16,3 +16,17 @@ if (playerChoice === computerChoice){
 } else {
     console.log("Invalid Input, R for Rock. P for Paper. S for Scissors");
 }
+
+
+//using ternary operators instead of if statements
+
+let options = "PRS";
+let playerChoice = prompt("P for Paper, R for Rock, S for Scissors").toUpperCase();
+let computerChoice = options.charAt(Math.floor(Math.random() * 3));
+console.log(`Player choses ${playerChoice}`);
+console.log(`Computer chooses ${computerChoice}`);
+playerChoice === computerChoice?"It's a tie!": 
+playerChoice === "R" && computerChoice ==="S"||
+playerChoice==="P" && computerChoice==="R"||
+playerChoice==="S" && computerChoice === "P"?
+"Player Wins": "Computer Wins";
